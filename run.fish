13498b4c7d89010e0f784aa9f8ad1e1f2bfa58e1
Salvador Guzman <guzmansalv@gmail.com>
2025-12-23 08:10:01 -0600

    hub

--- run.fish
+++ run.fish
@@ -4,7 +4,7 @@ docker run -it --user 0 --gpus all --name e2a-gui -p 7860:7860 \
     -v "/home/admin/Documents/tts/voices:/app/voices" \
     -v "/home/admin/Documents/tts/cache:/opt/cache" \
     -v "/drive/cache/dot-cache/uv:/opt/cache/uv" \
-    -v "/drive/cache/dot-cache/huggingface:/app/ebook2audiobook/models/tts" \
+    -v "/drive/cache/dot-cache/huggingface/hub:/app/ebook2audiobook/models/tts" \
     -v "/drive/AI/dicdir:/opt/venv/lib/python3.12/site-packages/unidic/dicdir:ro" \
     -v "/drive/AI/stanza:/app/ebook2audiobook/models/stanza" \
     -v (pwd)/app.py:/app/ebook2audiobook/app.py:ro \
